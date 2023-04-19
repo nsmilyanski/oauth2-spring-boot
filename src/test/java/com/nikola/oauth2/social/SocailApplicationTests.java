30b2bfe76bbdb1f565069cc3f5d969e8b54417c1
smilynik <nikola_smilyanski@flutterint.com>
2023-04-19 13:56:10 +0300

    first commit

---
+++ src/test/java/com/nikola/oauth2/social/SocailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikola.oauth2.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
